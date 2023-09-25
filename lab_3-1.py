4a22c4241ca3839a787e9b7e2b2945e9fdc8f939
Rrinvil <144148135+Rrinvil@users.noreply.github.com>
2023-09-25 09:15:17 -0400

    Add files via upload

---
+++ lab_3-1.py
@@ -0,0 +1,10 @@
+a= input("magic_charge")
+a = int (a)
+b= input("shield_charge")
+b= int (b)
+
+if (a >= 90 and b >= 75):
+	print ("You defeated the dragon! But the princess is in another castle.")
+else:
+	print ("The dragon burns you to a crisp.")
+	
